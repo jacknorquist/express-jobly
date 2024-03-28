a823499889dc6febc28b55dd150b42ac5411a6e2
Cooper Spinelli <cooperspinelli@gmail.com>
2024-03-28 14:42:59 -0600

    Finish job model and tests for job medel

--- models/jobs.js
+++ models/jobs.js
@@ -2,11 +2,18 @@
 
 const db = require("../db");
 const { BadRequestError, NotFoundError } = require("../expressError");
+const { sqlForPartialUpdate } = require("../helpers/sql");
 
 
 class Job {
 
-  /** TODO: write this */
+  /** Create a job (from data), update db, return new job data.
+   *
+   * data should be { title, salary, equity, company_handle }
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   */
+
   static async create({ title, salary, equity, companyHandle }) {
     const companyExistsCheck = await db.query(`
         SELECT handle
@@ -41,6 +48,14 @@ class Job {
     return job;
   }
 
+
+  /** Given a job id, return data about job.
+     *
+     * Returns { id, title, salary, equity, company_handle }
+     *
+     * Throws NotFoundError if not found.
+     **/
+
   static async get(jobId) {
     const result = await db.query(
       `SELECT title, salary, equity, company_handle as "companyHandle", id
@@ -49,15 +64,78 @@ class Job {
       [jobId]
     );
     const job = result.rows[0];
-    console.log(job);
 
     if (!job) throw new NotFoundError(`No company id: ${jobId}`);
 
     return job;
   }
 
+  /** Find all jobs
+   *
+   * Returns [{ id, title, salary, equity, company_handle }, ...]
+   **/
+
+  static async getAll() {
+    const result = await db.query(`
+          SELECT title, salary, equity, company_handle as "companyHandle", id
+          FROM jobs
+          ORDER BY title`,
+    );
+
+    return result.rows;
+  }
 
 
+  /** Update job data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain all the
+   * fields; this only changes provided ones.
+   *
+   * Data can include: { title, salary, equity, company_handle }
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   *
+   * Throws NotFoundError if not found.
+   */
+
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(
+      data, { companyHandle: "company_handle" });
+    const IdVarIdx = "$" + (values.length + 1);
+
+    const querySql = `
+        UPDATE jobs
+        SET ${setCols}
+        WHERE id = ${IdVarIdx}
+        RETURNING
+            title,
+            salary,
+            equity,
+            company_handle as "companyHandle",
+            id`;
+    const result = await db.query(querySql, [...values, id]);
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+
+  /** Delete given job from database; returns undefined.
+   *
+   * Throws NotFoundError if job not found.
+   **/
+
+  static async remove(jobId) {
+    const result = await db.query(`
+          DELETE
+          FROM jobs
+          WHERE id = $1
+          RETURNING id`, [jobId]);
+    const job = result.rows[0];
+    console.log(job);
+    if (!job) throw new NotFoundError(`No company: ${jobId}`);
+  }
 
 }
 
--- models/jobs.test.js
+++ models/jobs.test.js
@@ -18,7 +18,6 @@ afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
 
-
 /************************************** create */
 
 describe("create", function () {
@@ -72,7 +71,6 @@ describe("create", function () {
 describe("get", function () {
 
   test("works", async function () {
-    console.log(testJob[0]);
     let job = await Job.get(testJob[0].id);
     expect(job).toEqual(testJob[0]);
   });
@@ -86,4 +84,107 @@ describe("get", function () {
     }
   });
 
+});
+
+/************************************** get all */
+
+describe("get all job", function () {
+
+  test("works", async function () {
+    let jobs = await Job.getAll();
+    expect(jobs).toEqual([
+      {
+        companyHandle: "c1",
+        equity: "0",
+        id: 1,
+        salary: 15000,
+        title: "Job Test1",
+      }
+    ]);
+  });
+
+});
+
+/************************************** update */
+
+describe("update", function () {
+  const updateData = {
+    title: "worker2",
+    salary: 200000,
+    equity: "0",
+    companyHandle: 'c1',
+  };
+
+  test("works", async function () {
+    let job = await Job.update(testJob[0].id, updateData);
+    delete job.id;
+    expect(job).toEqual(updateData);
+
+    const result = await db.query(
+      `SELECT title, salary, equity, company_handle as "companyHandle"
+           FROM jobs
+           WHERE id = $1`, [testJob[0].id]);
+    expect(result.rows[0]).toEqual(updateData);
+  });
+
+  test("works: null fields", async function () {
+    const updateDataSetNulls = {
+    title: "worker3",
+    salary: null,
+    equity: null,
+    companyHandle: 'c1',
+    };
+
+    let job = await Job.update(testJob[0].id, updateDataSetNulls);
+    expect(job).toEqual({
+      id: testJob[0].id,
+      ...updateDataSetNulls,
+    });
+
+    const result = await db.query(
+      `SELECT title, salary, equity, company_handle as "companyHandle"
+           FROM jobs
+           WHERE id = $1`, [testJob[0].id]);
+    expect(result.rows[0]).toEqual(updateDataSetNulls);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.update(100000, updateData);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("bad request with no data", async function () {
+    try {
+      await Job.update(testJob[0].id, {});
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+
+
+});
+
+/************************************** remove */
+
+describe("remove", function () {
+  test("works", async function () {
+    await Job.remove(testJob[0].id);
+    const res = await db.query(
+      "SELECT id FROM jobs WHERE id=$1", [testJob[0].id]);
+    expect(res.rows.length).toEqual(0);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.remove(100000000);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
 });
